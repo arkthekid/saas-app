df83d05ae33e4e5c4dea643bddb5da4c24a282cc
Ark <arkarmoemyint100@gmail.com>
2025-11-29 23:38:40 -0500

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Companion Session
+    </div>
+  )
+}
+
+export default page
---
+++ app/companions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Companion Library
+    </div>
+  )
+}
+
+export default page
